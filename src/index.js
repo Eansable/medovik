e9723e9a8120e12d654c7e2b3a7bce469530c141
Andrei Tarasevich <tarasevich_a@ddc.by>
2024-02-09 16:23:37 +0300

    start work with about cafe section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import menuItems from './menu';
 import './styles.css'
 import './variables.css'
+import cafes from './cafes';
 
 const nextPage = (event) => {
     if (page === menuItems.length)
@@ -39,3 +40,34 @@ menuItems.forEach((item, index) => {
 })
 
 fsCarusel.appendChild(caruselItems)
+
+const nextDoor = document.getElementById('next_door')
+nextDoor.addEventListener('click', () => {
+    document.querySelector('.about_cafe').classList.add('about_cafe_hidden')
+    setTimeout(() => {
+        pageDoor + 1 === cafes.length ? renderAboutCafe(0) : renderAboutCafe(pageDoor + 1)
+    }, 400)
+})
+
+const prevDoor = document.getElementById('prev_door')
+prevDoor.addEventListener('click', () => {
+    document.querySelector('.about_cafe').classList.add('about_cafe_hidden')
+    setTimeout(() => {
+        pageDoor === 0 ? renderAboutCafe(cafes.length - 1) : renderAboutCafe(pageDoor - 1)
+    }, 400)
+})
+
+const nameCafe = document.getElementById('cafe_name')
+const aboutCafe = document.getElementById('cafe_about_text')
+
+let pageDoor
+
+const renderAboutCafe = (page) => {
+    document.querySelector('.about_cafe').classList.remove('about_cafe_hidden')
+    pageDoor = page
+    const cafe = cafes[pageDoor]
+    nameCafe.innerText = cafe.name
+    aboutCafe.innerText = cafe.about
+}
+
+renderAboutCafe(0)
