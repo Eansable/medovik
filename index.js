49ece4069d251e83bc3ccfb7a062fd1fba000e67
Andrei Tarasevich <tarasevich_a@ddc.by>
2024-08-30 15:56:25 +0300

    add button menu

--- index.js
+++ index.js
@@ -1,8 +1,103 @@
+const menu = [
+    {
+        name: "Медовик-кофе",
+        activeColor: "coffee",
+        photo: "",
+        top: "calc(100% - 180px)",
+        left: "500px",
+    },
+    {
+        name: "Медовик - соленая карамель",
+        activeColor: "salty",
+        photo: "",
+        top: "calc(100% - 300px)",
+        left: "500px",
+    },
+    {
+        name: "Медовик-лимон",
+        activeColor: "lemon",
+        photo: "",
+        top: "400px",
+        left: "1140px",
+    },
+    {
+        name: "Медовик - шоколадно-ореховый",
+        activeColor: "nut",
+        photo: "",
+        top: "285px",
+        left: "964px",
+    },
+    {
+        name: "Медовик-черника",
+        activeColor: "blueberry",
+        photo: "",
+        top: "175px",
+        left: "910px",
+    },
+    {
+        name: "Чизкейк",
+        activeColor: "cheese",
+        photo: "",
+        top: "80px",
+        left: "810px",
+    },
+    {
+        name: "Медовик классический",
+        activeColor: "classic",
+        photo: "",
+        top: "80px",
+        left: "520px",
+    },
+    {
+        name: "Медовик-кокос",
+        activeColor: "coconut",
+        photo: "",
+        top: "calc(100% - 685px)",
+        left: "360px",
+    },
+    {
+        name: "Медовик-малина",
+        activeColor: "raspberry",
+        photo: "",
+        top: "calc(100% - 595px)",
+        left: "200px",
+    },
+    {
+        name: "Медовик-вишня",
+        activeColor: "cherry",
+        photo: "",
+        top: "calc(100% - 465px)",
+        left: "100px",
+    },
+    {
+        name: "Наполеон",
+        activeColor: "napoleon",
+        photo: "",
+        top: "calc(100% - 360px)",
+        left: "79px",
+    },
+    {
+        name: "Наполеон - солёная карамель",
+        activeColor: "napoleon_salty",
+        photo: "",
+        top: "calc(100% - 240px)",
+        left: "0px",
+    },
+
+
+]
+
+
 const overlayModal = document.querySelector(".modal_overlay")
 const modal = document.querySelector(".modal")
 const buttons = document.querySelectorAll(".order")
 const modalButton = document.querySelector(".modal_order")
 const form = document.getElementById("order_form")
+const menuWrapper = document.querySelector(".menu_wrapper")
+
+
+// MODAL AND TELEGRAM
+
 const token = "7402101933:AAG8R-TlNh9UvQiMCm0S97m5CQ_-5nvQsDI"
 const chatId = "-1002231985778"
 const api = `https://api.telegram.org/bot${token}/sendMessage`
@@ -18,7 +113,6 @@ const openModal = (event) => {
 
 const sendForm = async (event) => {
     const fd = new FormData(form)
-    console.log(fd);
     try {
         const response = await fetch(api, {
             method: 'POST',
@@ -45,3 +139,7 @@ buttons.forEach(button => {
 })
 
 modalButton.addEventListener("click", sendForm)
+
+
+// MENU
+
