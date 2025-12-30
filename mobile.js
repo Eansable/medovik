577a024e97b4b8cfe21b2c313bf52d140a456bc8
andrei tarasevich <eansable@yandex.by>
2025-12-30 16:39:46 +0300

    feat: home page and add mobile images format

--- mobile.js
+++ mobile.js
@@ -1,3 +1,12 @@
+const medoviki = [
+  {
+    id: 1,
+    name: "Кофейный",
+    price: 50,
+    image: "./img/mobile/cakes/coffee.webp",
+  },
+];
+
 const content = document.querySelector(".content_mobile");
 
 const menu = document.createElement("div");
@@ -6,13 +15,22 @@ menu.classList.add("mobile_menu");
 const page = document.createElement("div");
 page.classList.add("page");
 
-const clickHandler = () => {};
+const changePage = (child) => {
+  return () => {
+    page.innerText = "";
+    page.appendChild(child);
+  };
+};
+
+const medovikiList = document.createElement("div");
+medovikiList.classList.add("medoviki_list");
+medovikiList.innerHTML = "<h2>MEDOVIKI:<h2>";
 
 const home = document.createElement("div");
 home.classList.add("home");
 home.innerHTML = `
 <header>
-  <img src="./img/mobile/logo-with-map.svg">
+  <img src="./img/mobile/logo-with-map.svg" class="logo_mobile">
   <div>
   <img src="./img/mobile/super-edovik.svg">
   </div>
@@ -29,11 +47,17 @@ home.innerHTML = `
 </a>
 </aside>
 <p>Мы — команда ценителей настоящих домашних десертов.<br>
-Наша цель — подарить каждому клиенту незабываемый вкус и радость от каждого кусочка наших медовиков.</p>`;
-
-const medovikiList = document.createElement("div");
-medovikiList.classList.add("medoviki_list");
-medovikiList.innerHTML = "<h2>MEDOVIKI:<h2>";
+Наша цель — подарить каждому клиенту незабываемый вкус и радость от каждого кусочка наших медовиков.</p>
+<div>
+<img src="./img/mobile/eco.svg" class="eco">
+<p class="eco_text">Благодаря использованию исключительно натуральных ингредиентов,
+мы абсолютно уверены в качестве наших десертов</p>
+</div>`;
+const takeOrder = document.createElement("button");
+takeOrder.classList.add("button_mobile");
+takeOrder.innerHTML = "Оформить заказ";
+takeOrder.addEventListener("click", changePage(medovikiList));
+home.appendChild(takeOrder);
 
 const yourChoice = document.createElement("div");
 yourChoice.classList.add("your_choice");
@@ -47,51 +71,26 @@ const contacts = document.createElement("div");
 contacts.classList.add("mobile_contacts");
 contacts.innerHTML = "<h2>WE ARE HERE:<h2>";
 
-const clickMedovikiHandler = () => {
-  page.innerText = "";
-  page.appendChild(medovikiList);
-};
-
-const clickHomeHandler = () => {
-  page.innerText = "";
-  page.appendChild(home);
-};
-
-const clickYourChoiceHandler = () => {
-  page.innerText = "";
-  page.appendChild(yourChoice);
-};
-
-const clickBucketHandler = () => {
-  page.innerText = "";
-  page.appendChild(bucket);
-};
-
-const clickContactsHandler = () => {
-  page.innerText = "";
-  page.appendChild(contacts);
-};
-
 const menuItems = [
   {
     icon: "./img/mobile/home.svg",
-    function: clickHomeHandler,
+    function: changePage(home),
   },
   {
     icon: "./img/mobile/heart.svg",
-    function: clickYourChoiceHandler,
+    function: changePage(yourChoice),
   },
   {
     icon: "./img/mobile/shopping-cart.svg",
-    function: clickBucketHandler,
+    function: changePage(bucket),
   },
   {
     icon: "./img/mobile/cake.svg",
-    function: clickMedovikiHandler,
+    function: changePage(medovikiList),
   },
   {
     icon: "./img/mobile/marker.svg",
-    function: clickContactsHandler,
+    function: changePage(contacts),
   },
 ];
 
